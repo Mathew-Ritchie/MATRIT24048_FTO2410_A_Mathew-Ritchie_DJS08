964c3d26655dfb1b663c78deda62e1e8b360ba12
Mathew Ritchie <ritchmat89@gmail.com>
2025-04-05 12:45:51 +0200

    added links to the van cards to the specific vans description page.

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Vans from "./pages/Vans.jsx";
+import VanDetail from "./pages/VanDetail";
 import "./server";
 
 export default function App() {
@@ -36,6 +37,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
